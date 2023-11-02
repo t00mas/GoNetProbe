018af23cf92e0fd6e523b2ffd027ee5da07829ae
t00mas <tomas.pica@redsift.io>
2023-11-02 10:29:52 +0100

    Extract TCP logic

--- scan.go
+++ scan.go
@@ -7,10 +7,27 @@ import (
 	"time"
 )
 
+type ScanResult struct {
+	Port int
+	Open bool
+}
+
 // ScanPort scans a port
 func ScanPort(dialer Dialer, protocol, hostname string, port int) bool {
+	switch protocol {
+	case "tcp":
+		return TCPScan(dialer, hostname, port)
+	case "udp":
+		fmt.Println("UDP scanning not implemented yet")
+		return false
+	default:
+		return false
+	}
+}
+
+func TCPScan(dialer Dialer, hostname string, port int) bool {
 	address := hostname + ":" + strconv.Itoa(port)
-	conn, err := dialer.DialTimeout(protocol, address, 60*time.Second)
+	conn, err := dialer.DialTimeout("tcp", address, 60*time.Second)
 	if err != nil {
 		return false
 	}
@@ -18,58 +35,47 @@ func ScanPort(dialer Dialer, protocol, hostname string, port int) bool {
 	return true
 }
 
-// InitialScan scans the first 1024 ports
-func InitialScan(dialer Dialer, hostname string) {
+func Scan(dialer Dialer, protocol, hostname string, startingPort, endingPort int) []ScanResult {
 	var wg sync.WaitGroup
 	ports := make(chan int)
+	results := make(chan ScanResult)
 
 	// Start the workers
 	for i := 0; i < 100; i++ {
 		go func() {
 			defer wg.Done()
 			for port := range ports {
-				result := ScanPort(dialer, "tcp", hostname, port)
-				if result {
-					fmt.Println("tcp port", port, "is open")
-				}
+				result := ScanPort(dialer, protocol, hostname, port)
+				results <- ScanResult{Port: port, Open: result}
 			}
 		}()
 	}
 
 	// Send ports to be scanned
-	for i := 0; i <= 1024; i++ {
+	for i := startingPort; i <= endingPort; i++ {
 		wg.Add(1)
 		ports <- i
 	}
 
 	close(ports)
 	wg.Wait()
+
+	// Collect the results
+	var scanResults []ScanResult
+	close(results)
+	for result := range results {
+		scanResults = append(scanResults, result)
+	}
+
+	return scanResults
+}
+
+// InitialScan scans the first 1024 ports
+func InitialTCPScan(dialer Dialer, hostname string) []ScanResult {
+	return Scan(dialer, "tcp", hostname, 1, 1024)
 }
 
 // WideScan scans the first 49152 ports
-func WideScan(dialer Dialer, hostname string) {
-	var wg sync.WaitGroup
-	ports := make(chan int)
-
-	// Start the workers
-	for i := 0; i < 100; i++ {
-		go func() {
-			defer wg.Done()
-			for port := range ports {
-				result := ScanPort(dialer, "tcp", hostname, port)
-				if result {
-					fmt.Println("tcp port", port, "is open")
-				}
-			}
-		}()
-	}
-
-	// Send ports to be scanned
-	for i := 0; i <= 49152; i++ {
-		wg.Add(1)
-		ports <- i
-	}
-
-	close(ports)
-	wg.Wait()
+func WideTCPScan(dialer Dialer, hostname string) []ScanResult {
+	return Scan(dialer, "tcp", hostname, 1, 49152)
 }
--- scan_test.go
+++ scan_test.go
@@ -4,15 +4,15 @@ import (
 	"testing"
 )
 
-func TestScanPort(t *testing.T) {
+func TestTCPScan(t *testing.T) {
 	// Test with a mock dialer that succeeds
-	result := ScanPort(&MockDialer{shouldSucceed: true}, "tcp", "localhost", 80)
+	result := TCPScan(&MockDialer{shouldSucceed: true}, "localhost", 80)
 	if !result {
 		t.Errorf("Expected true, got %v", result)
 	}
 
 	// Test with a mock dialer that fails
-	result = ScanPort(&MockDialer{shouldSucceed: false}, "tcp", "localhost", 80)
+	result = TCPScan(&MockDialer{shouldSucceed: false}, "localhost", 80)
 	if result {
 		t.Errorf("Expected false, got %v", result)
 	}
