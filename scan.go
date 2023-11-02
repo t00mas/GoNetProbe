fabb9d890c329a7f400d2dcc4826aabb939ea161
t00mas <tomas.pica@redsift.io>
2023-11-02 09:49:20 +0100

    Add comments

--- scan.go
+++ scan.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// ScanPort scans a port
 func ScanPort(dialer Dialer, protocol, hostname string, port int) bool {
 	address := hostname + ":" + strconv.Itoa(port)
 	conn, err := dialer.DialTimeout(protocol, address, 60*time.Second)
@@ -17,6 +18,7 @@ func ScanPort(dialer Dialer, protocol, hostname string, port int) bool {
 	return true
 }
 
+// InitialScan scans the first 1024 ports
 func InitialScan(dialer Dialer, hostname string) {
 	var wg sync.WaitGroup
 	for i := 0; i <= 1024; i++ {
@@ -32,6 +34,7 @@ func InitialScan(dialer Dialer, hostname string) {
 	wg.Wait()
 }
 
+// WideScan scans the first 49152 ports
 func WideScan(dialer Dialer, hostname string) {
 	var wg sync.WaitGroup
 	for i := 0; i <= 49152; i++ {
