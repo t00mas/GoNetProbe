6ae7395b9fe72482c05467fcb20defd52e99c29d
t00mas <tomas.pica@redsift.io>
2023-11-02 09:52:45 +0100

    Add workers & channels

--- scan.go
+++ scan.go
@@ -21,31 +21,55 @@ func ScanPort(dialer Dialer, protocol, hostname string, port int) bool {
 // InitialScan scans the first 1024 ports
 func InitialScan(dialer Dialer, hostname string) {
 	var wg sync.WaitGroup
+	ports := make(chan int)
+
+	// Start the workers
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer wg.Done()
+			for port := range ports {
+				result := ScanPort(dialer, "tcp", hostname, port)
+				if result {
+					fmt.Println("tcp port", port, "is open")
+				}
+			}
+		}()
+	}
+
+	// Send ports to be scanned
 	for i := 0; i <= 1024; i++ {
 		wg.Add(1)
-		go func(port int) {
-			defer wg.Done()
-			result := ScanPort(dialer, "tcp", hostname, port)
-			if result {
-				fmt.Println("tcp port", port, "is open")
-			}
-		}(i)
+		ports <- i
 	}
+
+	close(ports)
 	wg.Wait()
 }
 
 // WideScan scans the first 49152 ports
 func WideScan(dialer Dialer, hostname string) {
 	var wg sync.WaitGroup
+	ports := make(chan int)
+
+	// Start the workers
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer wg.Done()
+			for port := range ports {
+				result := ScanPort(dialer, "tcp", hostname, port)
+				if result {
+					fmt.Println("tcp port", port, "is open")
+				}
+			}
+		}()
+	}
+
+	// Send ports to be scanned
 	for i := 0; i <= 49152; i++ {
 		wg.Add(1)
-		go func(port int) {
-			defer wg.Done()
-			result := ScanPort(dialer, "tcp", hostname, port)
-			if result {
-				fmt.Println("tcp port", port, "is open")
-			}
-		}(i)
+		ports <- i
 	}
+
+	close(ports)
 	wg.Wait()
 }
